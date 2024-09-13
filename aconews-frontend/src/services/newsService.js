7f0e08727e1e86a31578b39b05de22450e51bc98
LuseBiswas <lusebiswas@gmail.com>
2024-09-13 13:43:46 +0530

    Updating newsService.js

--- aconews-frontend/src/services/newsService.js
+++ aconews-frontend/src/services/newsService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchNews = async (query, page = 1) => {
   try {
-    const response = await axios.get(`http://localhost:5783/api/news?query=${query}&page=${page}`);
+    const response = await axios.get(`https://aconews-backend-ji41.onrender.com/api/news?query=${query}&page=${page}`);
     return response.data;
   } catch (error) {
     console.error('Error fetching news:', error);
