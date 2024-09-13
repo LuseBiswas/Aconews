ee0b03e776f431502423962b151c01a625e46e93
LuseBiswas <lusebiswas@gmail.com>
2024-09-13 13:35:03 +0530

    All is fixed

--- aconews-backend/utils/fetchNews.js
+++ aconews-backend/utils/fetchNews.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import { config } from '../config/config.js';
 
-export const fetchNews = async (req, res) => {
-  const { query } = req.query;
-  const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
-  const pageSize = 10; // Number of articles per page
-
+export const fetchNews = async (query) => {
   try {
-    const response = await axios.get(`${BASE_URL}?q=${query}&apikey=${API_KEY}&page=${page}&max=${pageSize}`);
-    res.status(200).json(response.data);
+    const response = await axios.get(`${config.GNEWS_API_URL}/search`, {
+      params: {
+        q: query,
+        token: config.GNEWS_API_KEY
+      }
+    });
+    return response.data;
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching news from GNews API', error: error.message });
+    throw new Error('Error fetching news data');
   }
 };
